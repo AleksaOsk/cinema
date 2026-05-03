2c74dd172fcbd6d2edd0eabcfccb26228ff76b50
Aleksaa <ao@aleksa-oskina.ru>
2026-05-03 16:01:41 +0300

    diploma, first iteration

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import AdminPage from './pages/AdminPage';
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/cinema">
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/admin-login" element={<AdminLoginPage/>}/>
